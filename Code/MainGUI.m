fe865fb1946bc23fbd9ce36b39a37b93157c313b
Brian Jackson <bjack205@gmail.com>
2015-07-03 08:30:49 -0600

    Crop Scan with incomplete last row

--- Code/MainGUI.m
+++ Code/MainGUI.m
@@ -200,6 +200,8 @@ if name ~= 0
         handles.Settings.CI = ScanFileData{7};
         handles.Settings.Fit = ScanFileData{10};
         handles.Settings.ScanFilePath = fullfile(path,name);
+        
+        handles.Settings = CropScan(handles.Settings);
         handles.ScanFileLoaded = true;
         MaterialPopup_Callback(handles.MaterialPopup, [], handles);
         handles = guidata(handles.MainGUI);
@@ -584,4 +586,32 @@ IndList = 1:length(List);
 Value = IndList(strcmp(List,String));
 if isempty(Value); Value =1; end;
 set(Popup, 'Value', Value);
+
+function Settings = CropScan(Settings)
+%Unique x and y
+X = unique(Settings.XData);
+Y = unique(Settings.YData);
+
+%Number of steps in x and y
+Nx = length(X);
+Ny = length(Y);
+
+%Check if complete grid
+if (Nx * Ny) ~= Settings.ScanLength && ...
+        sum(Settings.YData == Y(end)) ~= sum(Settings.YData == Y(1)) && ...
+        sum(Settings.YData == Y(end-1)) == sum(Settings.YData == Y(1))
+    disp('Incomplete Grid, cropping last row...')
+    Ny = Ny - 1;
+    Settings.ScanLength = Nx * Ny;
+    Settings.Angles = Settings.Angles(1:Settings.ScanLength,:);
+    Settings.XData = Settings.XData(1:Settings.ScanLength,:);
+    Settings.YData = Settings.YData(1:Settings.ScanLength,:);
+    Settings.IQ = Settings.IQ(1:Settings.ScanLength,:);
+    Settings.CI = Settings.CI(1:Settings.ScanLength,:);
+    Settings.Fit = Settings.Fit(1:Settings.ScanLength,:);
+end
+Settings.X = X; Settings.Nx = Nx;
+Settings.Y = Y; Settings.Ny = Ny;
+    
+
 
