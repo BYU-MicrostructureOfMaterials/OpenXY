75f8c9519750b8f2d1d38c95c561d386864a53ba
Brian Jackson <bjack205@gmail.com>
2015-07-25 09:28:34 -0600

    Added options to GetMaterialsList
    Added options to return different versions of the list

--- Code/GetMaterialsList.m
+++ Code/GetMaterialsList.m
@@ -1,8 +1,20 @@
-function materials = GetMaterialsList
+function materials = GetMaterialsList(option)
+% Option:
+%   1 = All materials and alteranate names and Auto-detect 
+%   2 = All materials and alternatve names
+%   anything else = All Materials (no alternative names and no Auto-detect)
+if nargin == 0
+    option = 1;
+end
 mats = dir('Materials');
 materials = {mats([mats.isdir] == 0).name}';
 for i = length(materials):-1:1
     [~,materials{i+1},~] = fileparts(materials{i});
 end
-materials{1} = 'Auto-detect';
-materials = [materials; {'ferrite';'austenite';'aluminium'}];
+materials = materials(2:end);
+if option == 1
+    materials{1} = 'Auto-detect';
+end
+if option == 1 || option == 2
+    materials = [materials; {'ferrite';'austenite';'aluminium'}];
+end
