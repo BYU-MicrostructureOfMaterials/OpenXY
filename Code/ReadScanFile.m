c8b270a3f7f6935c6dbe9902cd2eba33fda89c2e
Brian Jackson <bjack205@gmail.com>
2015-08-31 08:05:50 -0600

    Added HKL to OIM Conversion factor

--- Code/ReadScanFile.m
+++ Code/ReadScanFile.m
@@ -9,7 +9,7 @@ elseif strcmp(ext,'.ctf')
     ScanFileData(:,[1,2,3,4,5,6,7,8,9,10])=ScanFileData(:,[6,7,8,2,3,10,11,5,1,9]);
     ScanFileData = ScanFileData(:,1:end-1);
     %Convert angles to radians
-    ScanFileData{1} = ScanFileData{1}*(pi/180);
+    ScanFileData{1} = (ScanFileData{1}+90)*(pi/180);
     ScanFileData{2} = ScanFileData{2}*(pi/180);
     ScanFileData{3} = ScanFileData{3}*(pi/180);
 else
