7887e53dcf2d1f9ca188635645ff465735d4cec8
Brian Jackson <bjack205@gmail.com>
2015-11-10 08:41:27 -0700

    Save settings struct after error

--- Code/MainGUI.m
+++ Code/MainGUI.m
@@ -386,7 +386,7 @@ catch ME
     handles.ScanFileLoaded = false;
     Reset_RunButton(handles);
     enableRunButton(handles);
-    save('temp/ErrorSettings.mat');
+    save('temp/ErrorSettings.mat','Settings');
     msg = 'OpenXY encountered an error. Re-select the scan file to reset.';
     cause = MException('MATLAB:OpenXY',msg);
     ME = addCause(ME,cause);
