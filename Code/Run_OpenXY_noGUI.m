1fe7681be21679e1b67d8eb5568b9e008c9f3ea4
Brian Jackson <bjack205@gmail.com>
2016-01-16 15:41:56 -0700

    Read new scan info for each scan

--- Code/Run_OpenXY_noGUI.m
+++ Code/Run_OpenXY_noGUI.m
@@ -8,6 +8,7 @@ end
 %% Set up Settings
 Settings.HROIMMethod = 'Simulated'; %{'Simulated', 'Real', 'Dynamic Simulated'}
 Settings.DisplayGUI = 1;
+Settings.DoParallel = 3;
 Settings.Material = 'silicon_BEJ';
 Algorithms = {'fminsearch','crosscor'};  %{'fminsearch','pso','crosscor'};
 AlgorithmNames = {'StrainMin','Crosscor'};
@@ -21,17 +22,45 @@ for sc = 1:2
     scan = scans{sc};
     Settings.ScanFilePath = ScanData.(scan).ScanFilePath;
     Settings.FirstImagePath = ScanData.(scan).FirstImagePath;
+    
+    %Read Scan File
     [path, name, ext] = fileparts(Settings.ScanFilePath);
+    Settings = rmfield(Settings,'Angles');
     Settings = ImportScanInfo(Settings,[name ext],path);
     
+    %Read Grain File
+    Settings.grainID = []; Settings.Phase = [];
+    [Settings.grainID, Settings.Phase] = GetGrainInfo(Settings.ScanFilePath, Settings.Material, Settings.ScanParams, Settings.Angles, Settings.MisoTol);
     
+    %Get ImagesNamesList
+    X = unique(Settings.XData);
+    Y = unique(Settings.YData);
+
+    %Step size in x and y
+    if strcmp(Settings.ScanType,'Square')
+        XStep = X(2)-X(1);
+        if length(Y) > 1
+            YStep = Y(2)-Y(1);
+        else
+            YStep = 0; %Line Scans
+        end
+    else
+        XStep = X(3)-X(1);
+        YStep = Y(3)-Y(1);
+    end
+    %Get Image Names
+    if ~isempty(Settings.FirstImagePath)
+        Settings.ImageNamesList = GetImageNamesList(Settings.ScanType, ...
+            Settings.ScanLength,[Settings.Nx Settings.Ny], Settings.FirstImagePath, ...
+            [Settings.XData(1),Settings.YData(1)], [XStep, YStep]);
+    end
     
-    for al = 1:2
+    for al = 1:1
         Algorithm = Algorithms{al};
         AlgorithmName = AlgorithmNames{al};
         
         for cal = 1:2
-            CalPoints = Cals{cal,2};
+            CalPoints = ScanData.(scan).CalPoints(Cals{cal,2});
             CalName = Cals{cal,1};
             ScanName = [scan ' BEJ Kin ' AlgorithmName ' ' CalName '.ctf'];
             Settings.OutputPath = fullfile(ScanData.(scan).folder,ScanName);
@@ -46,8 +75,11 @@ for sc = 1:2
                         96 108 121;... i02 2nd band
                         204 220 231]; %i02 3rd band
             Settings.CalibrationPointIndecies = CalPoints;
-
             ScanParams = Settings.ScanParams;
+              
+            Settings.XStar = [];
+            Settings.YStar = [];
+            Settings.ZStar = [];
             if 0
                 ScanParams.xstar = Settings.ScanParams.xstar;
                 ScanParams.ystar = Settings.ScanParams.ystar;
