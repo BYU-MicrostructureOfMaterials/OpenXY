181dedced50ab34a305e078c0144269f05ce70e5
Brian Jackson <bjack205@gmail.com>
2016-05-25 14:02:04 -0600

    Orientation Error function

---
+++ Code/OrientationError.m
@@ -0,0 +1,13 @@
+function [MisAng,Correction] = OrientationError(Settings,ScanFilePath,orientation)
+ScanFileData = ReadScanFile(ScanFilePath);
+CorrectAngles = [ScanFileData{1:3}];
+
+MisAng = zeros(Settings.Nx,1);
+Correction = zeros(Settings.Nx,5);
+for i = 1:Settings.Nx
+    MisAng(i) = GeneralMisoCalc(CorrectAngles(i,:),Settings.NewAngles(i,:),'tetragonal');
+    for j = 1:5
+        Correction(i,j) = GeneralMisoCalc(euler2gmat(CorrectAngles(i,:)),orientation{i,j},'tetragonal');
+    end
+end
+plot(MisAng(1:100))
