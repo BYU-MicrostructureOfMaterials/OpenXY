b519b3f0341c351390569c8ba496551261894b3c
Brian Jackson <bjack205@gmail.com>
2016-05-30 14:49:02 -0600

    PlotIPF Function

---
+++ Code/PlotIPF.m
@@ -0,0 +1,13 @@
+function IPF_map = PlotIPF(g,dims,plot)
+if nargin < 3
+    plot = 1;
+end
+for i = 1:dims(1)*dims(2)
+    IPF(i,:) = IPF_rgbcalc(g(:,:,i));
+end
+IPF_map(:,:,1) = reshape(IPF(:,1),dims(2),dims(1));
+IPF_map(:,:,2) = reshape(IPF(:,2),dims(2),dims(1));
+IPF_map(:,:,3) = reshape(IPF(:,3),dims(2),dims(1));
+if plot
+    image(IPF_map)
+end
