d7450018fe53911169df34ef2157af2c1c4a9398
Brian Jackson <bjack205@gmail.com>
2016-06-09 14:35:49 -0600

    Vectorize Hex2Array
    Almost 500x faster for scan length of about 5 million points

--- Code/Hex2Array.m
+++ Code/Hex2Array.m
@@ -1,23 +1,6 @@
-function array = Hex2Array (vector, NColsOdd, NColsEven)
-
-r = 1;
-c = 1;
-for i = 1:length(vector)
-    
-    array(r,c) = vector(i);
-    c = c + 1;
-    if mod(r,2) == 1 %Odd
-        if c > NColsOdd
-            c = 1;
-            r = r + 1;
-        end
-    else %Even
-        if c > NColsEven
-            c = 1;
-            r = r + 1;
-        end
-    end
-end
-array = array(:,1:end-1);
-        
+function array = Hex2Array (vector, NColsOdd)
+NColsEven = NColsOdd-1;
+ScanLength = length(vector);
+vector(NColsOdd:NColsOdd+NColsEven:ScanLength) = [];
+array = reshape(vector,NColsEven,[])';
             
