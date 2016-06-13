ae5553034e114aea85ec0288d4d651ee5c0d9a1e
Brian Jackson <bjack205@gmail.com>
2016-06-13 14:15:56 -0600

    Add Line Scan Plotting

--- Code/PlotScan.m
+++ Code/PlotScan.m
@@ -1,4 +1,9 @@
 function PlotScan(im,PlotType)
+%Copy data for Line Scans
+if size(im,1) == 1
+    im = repmat(im,round(size(im,2)/6),1);
+end
+
 switch PlotType
     case 'Image Quality'
         imagesc(im)
