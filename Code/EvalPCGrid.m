fbd900f4c9bed331093ed18fc2a6b0dcd2aaed6f
Brian Jackson <bjack205@gmail.com>
2016-06-13 14:34:14 -0600

    Improve filtering on PCGrid

--- Code/EvalPCGrid.m
+++ Code/EvalPCGrid.m
@@ -1,22 +1,30 @@
 function [PCopt,pp] = EvalPCGrid(strainvals,pcvals,plots)
+thresh = 5e-3;
 if nargin < 3
     plots = 1;
 end
-[numpats,numpc] = size(strainvals);
-numlow = sum(strainvals<5e-3,1);
+[numpats] = size(strainvals);
+
+%Remove bad points
+ginds = median(strainvals,1)<thresh;
+strainvals_filt = strainvals(:,ginds);
+pcvals_filt = pcvals(:,ginds);
+numpc_filt = length(strainvals_filt);
+
+numlow = sum(strainvals_filt<thresh,1);
 [nlow, indlow] = max(numlow);
 
 %Pick out best patterns
-qq = strainvals(:,indlow)<5e-3;
+qq = strainvals_filt(:,indlow)<thresh;
 nn = 1:numpats;
 nn = nn(qq);
 
 %Get Median lines
-thismean = zeros(1,numpc);
-medianPC = zeros(1,numpc);
-for i=1:numpc
-    thismean(i)=median(strainvals(qq,i));
-    medianPC(i)=median(pcvals(qq,i));
+thismean = zeros(1,numpc_filt);
+medianPC = zeros(1,numpc_filt);
+for i=1:numpc_filt
+    thismean(i)=median(strainvals_filt(qq,i));
+    medianPC(i)=median(pcvals_filt(qq,i));
 end
 
 %Apply Quadratic Fit
@@ -28,7 +36,7 @@ minstrain = pp(1)*PCopt^2+pp(2)*PCopt+pp(3);
 if plots
     hold on
     for i=1:nlow
-        plot(pcvals(nn(i),:),strainvals(nn(i),:),'*'); 
+        plot(pcvals_filt(nn(i),:),strainvals_filt(nn(i),:),'*'); 
     end
     plot(medianPC,thismean); ylim([0 0.01]);
     plot(medianPC,PCfit,'k','LineWidth',1);
