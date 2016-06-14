1bcdb4559af959a0dfcfbdeb5e782b8a46293227
Brian Jackson <bjack205@gmail.com>
2016-06-14 10:41:10 -0600

    Accept horizontal or vertical vectors

--- Code/Hex2Array.m
+++ Code/Hex2Array.m
@@ -1,4 +1,7 @@
 function array = Hex2Array (vector, NColsOdd)
+if size(vector,1)<size(vector,2)
+    vector = vector';
+end
 NColsEven = NColsOdd-1;
 [ScanLength,width] = size(vector);
 vector(NColsOdd:NColsOdd+NColsEven:ScanLength,:) = [];
