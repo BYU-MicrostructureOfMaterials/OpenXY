af6749356b133eb4388b0eeaf6be50f7110bad4b
Brian Jackson <bjack205@gmail.com>
2016-06-14 10:43:22 -0600

    IQ map limits based on standard deviation

--- Code/PlotScan.m
+++ Code/PlotScan.m
@@ -4,9 +4,15 @@ if size(im,1) == 1
     im = repmat(im,round(size(im,2)/6),1);
 end
 
+StdDev = std(im(:));
+Mean = mean(im(:));
+Limits(1) = Mean - 3*StdDev;
+Limits(2) = Mean + 3*StdDev;
+
 switch PlotType
     case 'Image Quality'
         imagesc(im)
+        caxis(Limits)
         colormap gray
     case 'IPF'
         image(im)
