47e276fbe93b47102182b35a46cb39c9d3124013
Brian Jackson <bjack205@gmail.com>
2016-11-10 11:30:27 -0700

    Scan Cropping for Hexagonal Scans
    Fixed issue with trying to crop hexagonal scans with an even number of
    rows

--- Code/CropScan.m
+++ Code/CropScan.m
@@ -3,20 +3,42 @@ Nx = Settings.Nx;
 Ny = Settings.Ny;
 
 Y = unique(Settings.YData);
+cropped = false;
 
 %Check if complete grid
-if (Nx * Ny) ~= Settings.ScanLength && ...
-        sum(Settings.YData == Y(end)) ~= sum(Settings.YData == Y(1)) && ...
-        sum(Settings.YData == Y(end-1)) == sum(Settings.YData == Y(1))
+if strcmp(Settings.ScanType,'Square')
+    if (Nx * Ny) ~= Settings.ScanLength && ...
+            sum(Settings.YData == Y(end)) ~= sum(Settings.YData == Y(1)) && ... % Last row has different number of points than the first
+            sum(Settings.YData == Y(end-1)) == sum(Settings.YData == Y(1))      % Second to last row has same number of points as the first
+        Ny = Ny - 1;
+        Settings.ScanLength = Nx * Ny;
+        cropped = true;
+    end
+elseif strcmp(Settings.ScanType,'Hexagonal')
+    % Account for changing number of points per row
+    extra = 0;
+    if mod(Ny,2) == 0 % Even
+        extra = 1;
+    end
+    if HexLength(Nx,Ny) ~= Settings.ScanLength && ...
+            sum(Settings.YData == Y(end)) ~= (sum(Settings.YData == Y(1)) + extra) && ... % Last row has different number of points than the first
+            sum(Settings.YData == Y(end-1)) ~= (sum(Settings.YData == Y(1)) + extra)      % Second to last row has same number of points as the first
+        Ny = Ny - 1;
+        Settings.ScanLength = HexLength(Ny,Ny);
+        cropped = true;
+    end
+end
+if cropped
     disp('Incomplete Grid, cropping last row...')
-    Ny = Ny - 1;
-    Settings.ScanLength = Nx * Ny;
-    Settings.Angles = Settings.Angles(1:Settings.ScanLength,:);
-    Settings.XData = Settings.XData(1:Settings.ScanLength,:);
-    Settings.YData = Settings.YData(1:Settings.ScanLength,:);
-    Settings.IQ = Settings.IQ(1:Settings.ScanLength,:);
-    Settings.CI = Settings.CI(1:Settings.ScanLength,:);
-    Settings.Fit = Settings.Fit(1:Settings.ScanLength,:);
+    Inds = 1:Settings.ScanLength;
+    Settings.Angles = Settings.Angles(Inds,:);
+    Settings.XData = Settings.XData(Inds,:);
+    Settings.YData = Settings.YData(Inds,:);
+    Settings.IQ = Settings.IQ(Inds,:);
+    Settings.CI = Settings.CI(Inds,:);
+    Settings.Fit = Settings.Fit(Inds,:);
+    Settings.grainID = Settings.grainID(Inds,:);
+    Settings.Phase = Settings.Phase(Inds,:);
 end
 Settings.Nx = Nx;
 Settings.Ny = Ny;
