9ecee224b41f2b73e42e010cf2d94ec99d959603
Brian Jackson <bjack205@gmail.com>
2016-12-17 15:45:31 -0700

    Separate function for importing image names list

---
+++ Code/ImportImageNamesList.m
@@ -0,0 +1,23 @@
+function ImageNamesList = ImportImageNamesList(Settings)
+X = unique(Settings.XData);
+Y = unique(Settings.YData);
+
+%Step size in x and y
+if strcmp(Settings.ScanType,'Square')
+    XStep = X(2)-X(1);
+    if length(Y) > 1
+        YStep = Y(2)-Y(1);
+    else
+        YStep = 0; %Line Scans
+    end
+else
+    XStep = X(3)-X(1);
+    YStep = Y(3)-Y(1);
+end
+
+%Get Image Names
+if ~isempty(Settings.FirstImagePath)
+    ImageNamesList = GetImageNamesList(Settings.ScanType, ...
+        Settings.ScanLength,[Settings.Nx Settings.Ny], Settings.FirstImagePath, ...
+        [Settings.XData(1),Settings.YData(1)], [XStep, YStep]);
+end
