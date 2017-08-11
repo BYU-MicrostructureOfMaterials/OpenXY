d6d16d064915b925e4ec20a61f26a39b96a658b8
Zach Clayburn <zachclayburn@gmail.com>
2017-08-11 13:39:18 -0600

    More Documentation changes

--- Code/@OXY/OXY.m
+++ Code/@OXY/OXY.m
@@ -2,44 +2,47 @@ classdef OXY < handle
     %OXY Handle Object behind OpenXY
         
     properties
-        %% Main GUI
         
-        %The Path of the scan file (.ang, .ctf, or .h5)
+        %The Path of the scan file 
+        %   The scan file is the scan produced by either OIM or AZTEK(.ang,
+        %   .ctf of .h5)
         ScanFilePath@char
+        
         %The Path of the first image of the scan
+        %   This should be the the file corisponding to the first image in
+        %   the scan
         FirstImagePath@char
+        
         %The Path where the results will be saved
         OutputPath@char
+        
         %The type of scan, either 'Square' or 'Hexagonal'
         ScanType@char = 'Square'
+        
         %The Material of the scan. If 'Scan File' is selected, material will be read from the scan file
         Material@char = 'Scan File'
+        
         %The number of workers used for parallel processing. Defaults to one less than the available cores
         DoParallel@double scalar =feature('numCores')-1
+        
         %If true, Shifts will be visualized during cross-corelation
         DoShowPlot@logical = false
+        
         %If true, there is scan info saved in the .TIFF file
         ImageTag@logical = false
+        
         %If true, will display GUI elements during computations
         DisplayGUI@logical = true
         
-        %% ROI/Filter Settings
-        %ROI Settings
         
         ROISizePercent@double scalar = 25
         ROISize@double scalar
         NumROIs@double scalar = 48
         ROIStyle@char = 'Grid'
         ROIFilter@double vector = [2 50 1 1]
-        
-        %Filter Settings
-        
         ImageFilterType@char = 'standard'
         ImageFilter@double vector = [9 90 0 0]
-        
-        %% Advanced Settings
-        %HROIM Settings
-        
+              
         DoStrain@logical = true
         HROIMMethod@char = 'Real'
         IterationLimit@double scalar = 6
@@ -49,37 +52,28 @@ classdef OXY < handle
         GrainRefImageType@char = 'IQ > Fit > CI'
         GrainMethod@char = 'Grain File'
         MinGrainSize@double scalar = 0
-        
-        %Dislocation Density Settings
-        
+                
         CalcDerivatives@logical = false
         GNDMethod@char = 'Full'
         NumSkipPts@double scalar = 0
         IQCutoff@double scalar = 0
         
-        %Split Dislocation Density
         
         DoDDS@logical = false
         DDSMethod@char = 'Nye-Kroner'
         
-        %Kernel Average Misorientation
         
         KernelAvgMisoPath@char
         
-        %Calculation Options
         
         EnableProfiler@logical = false
-        
-        %% PC Calibration
-        
+                
         PlaneFit@char = 'Naive'
         PCList@cell matrix
         XStar@double vector
         YStar@double vector
         ZStar@double vector
-        
-        %% Microscope Settings
-        
+                
         AccelVoltage@double scalar = 20
         SampleTilt@double scalar = 70*pi/180
         SampleAzimuthal@double scalar = 0
@@ -87,14 +81,10 @@ classdef OXY < handle
         CameraAzimuthal@double scalar = 0
         mperpix@double scalar = 25
         PhosphorSize@double scalar
-        
-        %% Status Variables
-        
+                
         HREBSDPrep@logical = false
         CalcMI@logical
-        
-        %% Scan Info
-        
+                
         ScanParams@struct
         GrainVals@struct
         ScanLength@double scalar
@@ -113,15 +103,12 @@ classdef OXY < handle
         ImageNamesList@cell vector
         Inds@double vector
         RefInd@double vector
-        
-        %% Old Variables
-        
+                
         DoUsePCFile@logical = false
         PCFilePath@char 
         FCalcMethod@char = 'Collin Crystal'
         largefftmeth@char
         
-        %% Ungrouped
     end
     
     methods
