73709c857815079d87955ecdeb4cb2aa37846692
Zach Clayburn <zachclayburn@gmail.com>
2017-08-15 15:35:05 -0600

    Set up SystemSettings object

---
+++ Code/@SystemSettings/SystemSettings.m
@@ -0,0 +1,13 @@
+classdef SystemSettings
+   
+    properties
+        %The Path of the OpenXY files
+        OpenXYPath
+        
+        %The position of the main gui
+        pos
+        
+        %
+    end
+    
+end
