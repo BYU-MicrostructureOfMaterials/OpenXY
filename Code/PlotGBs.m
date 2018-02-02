0830957b5a4a3bc64c048de958a80f193d872bd3
Zach Clayburn <zachclayburn@gmail.com>
2018-02-02 14:43:58 -0700

    Refining the PlotGBs function to be more efficient

--- Code/PlotGBs.m
+++ Code/PlotGBs.m
@@ -16,12 +16,13 @@ function PlotGBs(GrainMap,mapsize,Type,ax)
 if nargin > 1
     GrainMap = vec2map(GrainMap,mapsize(1),Type);
 end
-if nargin < 4
+if nargin < 4;
     ax = gca;
 end
 
-%Line Width
-lw = 2;
+%Line Properties
+lineWidth = 1;
+color = 'k';
 
 %Horizontal Boundaries
 diffMap = diff(GrainMap,1,1);
@@ -45,27 +46,27 @@ if ~(isempty(row) || isempty(col))
         head = [X(ii-1,2) Y(ii-1,2)];
         tail = [X(ii,1) Y(ii,1)];
         if ~isequal(head,tail)
-            % If one line ends at the begining of the next, skip that ending
-            % point and move on to the next point, otherwise, append them to the
-            % list of points
+            % If one line ends at the begining of the next, skip that
+            %   ending point and move on to the next point, otherwise,
+            %   append them to the list of points
             newX(jj-1,2) = head(1);
             newY(jj-1,2) = head(2);
             newX(jj,1) = tail(1);
             newY(jj,1) = tail(2);
             jj = jj+1;
-        end
-    end
+        end% ~isequal(head,tail)
+    end% ii = 2:length(X)
     
     newX(jj-1,2) = X(end);
     newY(jj-1,2) = Y(end);
-    X = newX(1:jj-1,:);
-    Y = newY(1:jj-1,:);
+    hX = newX(1:jj-1,:);
+    hY= newY(1:jj-1,:);
     
-    hold on
-    plot(ax,X',Y','k','LineWidth',lw)
-end
+    %hold on
+    %plot(ax,X',Y',color,'LineWidth',lineWidth)
+end% ~(isempty(row) || isempty(col))
 
-%Vertical
+%Vertical Boundaries
 diffMap = diff(GrainMap,1,2);
 [row,col] = find(diffMap);
 
@@ -81,25 +82,176 @@ if ~(isempty(row) || isempty(col))
     newX(1) = X(1);
     newY(1) = Y(1);
     jj = 2;
+    
     for ii = 2:length(X)
         head = [X(ii-1,2) Y(ii-1,2)];
         tail = [X(ii,1) Y(ii,1)];
         if ~isequal(head,tail)
-            % If one line ends at the begining of the next, skip that ending
-            % point and move on to the next point, otherwise, append them to the
-            % list of points
+            % If one line ends at the begining of the next, skip that
+            %   ending point and move on to the next point, otherwise,
+            %   append them to the list of points
             newX(jj-1,2) = head(1);
             newY(jj-1,2) = head(2);
             newX(jj,1) = tail(1);
             newY(jj,1) = tail(2);
             jj = jj+1;
-        end
-    end
+        end% ~isequal(head,tail)
+    end% ii = 2:length(X)
     
     newX(jj-1,2) = X(end);
     newY(jj-1,2) = Y(end);
-    X = newX(1:jj-1,:);
-    Y = newY(1:jj-1,:);
+    vX = newX(1:jj-1,:);
+    vY = newY(1:jj-1,:);
+        
+    lines = combineLines(hX,hY,vX,vY);
     
-    plot(ax,X',Y','k','LineWidth',lw)
-end
+    hold(ax,'on')
+    for ii = 1:size(lines,1)
+        plot(ax,lines{ii,1},lines{ii,2},'LineWidth',lineWidth,'Color',color)
+    end%ii = 1:size(lines,1)
+    hold(ax,'off')
+
+end%if ~(isempty(row) || isempty(col))
+end%PlotGBS
+
+
+function out = combineLines(hX,hY,vX,vY)
+% This function combines diagonal lines into one path to reduce the
+%   indvidual lines that Matlab has to draw and boost the performance when
+%   working with scans containing a large amount of grain boundaries
+
+% Take the vectors describing the x and y positions of the horizontal lines
+%   and organize them into matricies relating to the start and end 
+%   cartesian coordinates
+hStart = [hX(:,1) hY(:,1)];
+hEnd = [hX(:,2) hY(:,1)];
+
+% Do the same for the vertical lines
+vStart = [vX(:,1) vY(:,1)];
+vEnd = [vX(:,1) vY(:,2)];
+
+% Set up an output cell containing the the new combined lines, and create 
+%   an iterateor value to track where to place new cells
+out = cell(0,2);%TODO Should I preallocate an overly large array? Speed vs memory use
+row = 1;
+
+% First handle lines with a negative slope
+ii = 1;
+while(ii <= length(hStart))
+    currentEnd = hEnd(ii,:);
+    
+    % Check if the current point from the vertical lines are in the
+    %   horizontal lines, and get its position if it is
+    [isPresent,vLocation] = ismember(currentEnd,vStart,'rows');
+    hLocation = ii;
+    if isPresent && 1
+        % Begin output vectors for the combined line's x and y coordinates
+        xOut = [hStart(hLocation,1);hEnd(hLocation,1)];
+        yOut = [hStart(hLocation,2);hEnd(hLocation,2)];
+        
+        % Remove the point that is goint to be joined into the combined
+        %   line
+        hStart(hLocation,:) = [];
+        hEnd(hLocation,:) = [];
+        while isPresent
+            % Append the found point to the combined line
+            xOut(end+1) = vEnd(vLocation,1);
+            yOut(end+1) = vEnd(vLocation,2);
+            currentEnd = vEnd(vLocation,:);
+            
+            % Remove the point that is goint to be joined into the combined
+            %   line
+            vStart(vLocation,:) = [];
+            vEnd(vLocation,:) = [];
+            
+            [isPresent,hLocation] = ismember(currentEnd,hStart,'rows');
+            if isPresent
+                currentEnd = hEnd(hLocation,:);
+                
+                % Append the found point to the combined line
+                xOut(end+1) = hEnd(hLocation,1);
+                yOut(end+1) = hEnd(hLocation,2);
+                
+                % Remove the point that is goint to be joined into the combined
+                %   line
+                hStart(hLocation,:) = [];
+                hEnd(hLocation,:) = [];
+
+                [isPresent,vLocation] = ismember(currentEnd,vStart,'rows');
+            end%if isPresent
+        end%while isPresent
+        out(row,:) = {xOut,yOut};
+        row = row + 1;
+    else
+        ii = ii + 1;
+    end%any(isPresent)
+end%while(ii <= length(hStart))
+
+% Now handle lines with a positive slope
+ii = 1;
+while(ii <= length(hStart))
+    currentStart = hStart(ii,:);
+    
+    % Check if the current point from the horizontal lines are in the 
+    %   vertical lines, and get its position if it is
+    [isPresent,vLocation] = ismember(currentStart,vStart,'rows');
+    hLocation = ii;
+    if isPresent && 1
+        % Begin output vectors for the combined line's x and y coordinates
+        xOut = [hStart(hLocation,1);hEnd(hLocation,1)];
+        yOut = [hStart(hLocation,2);hEnd(hLocation,2)];
+        
+        % Remove the point that is goint to be joined into the combined
+        %   line
+        hStart(hLocation,:) = [];
+        hEnd(hLocation,:) = [];
+        while isPresent
+            % Append the found point to the combined line
+            xOut = [vEnd(vLocation,1); xOut];
+            yOut = [vEnd(vLocation,2); yOut];
+            currentStart = vEnd(vLocation,:);
+            
+            % Remove the point that is goint to be joined into the combined
+            %   line
+            vStart(vLocation,:) = [];
+            vEnd(vLocation,:) = [];
+            
+            [isPresent,hLocation] = ismember(currentStart,hEnd,'rows');
+            if isPresent
+                currentStart = hStart(hLocation,:);
+
+                % Append the found point to the combined line
+                xOut = [hStart(hLocation,1); xOut];
+                yOut = [hStart(hLocation,2); yOut];
+
+                % Remove the point that is goint to be joined into the combined
+                %   line
+                hStart(hLocation,:) = [];
+                hEnd(hLocation,:) = [];
+
+                [isPresent,vLocation] = ismember(currentStart,vStart,'rows');
+            end%if isPresent
+        end%while isPresent
+        
+        out(row,:) = {xOut,yOut};
+        row = row + 1;
+    else
+        ii = ii + 1;
+    end%any(isPresent)
+end%while(ii <= length(hStart))
+
+% Add all of the leftover horizontal lines to the output cell
+for ii = 1:length(hStart)
+    row = row + 1;
+    out(row,1) = {[hStart(ii,1);hEnd(ii,1)]};
+    out(row,2) =  {[hStart(ii,2);hEnd(ii,2)]};
+end%ii = 0:length(hStart)
+
+% Add all of the leftover vertical lines to the output cell
+for ii = 1:length(vStart)
+    row = row + 1;
+    out(row,1) = {[vStart(ii,1);vEnd(ii,1)]};
+    out(row,2) =  {[vStart(ii,2);vEnd(ii,2)]};
+end%ii = 0:length(vStart)
+
+end%combineLines
