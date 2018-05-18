d17aaa601431dd93e304ec061e61212d81f47013
Zach Clayburn <zachclayburn@gmail.com>
2018-05-18 11:51:29 -0600

    Images are now sent

--- Code/+superComp/@Batch/sendImages.m
+++ Code/+superComp/@Batch/sendImages.m
@@ -3,12 +3,18 @@ function sendImages(obj)
 disp('Sending Images')
 
 Settings = obj.Settings;
-path = fileparts(Settings.FirstImagePath);
-path = strsplit(path,filesep);
-folderName = path{end};
+localPath = fileparts(Settings.FirstImagePath);
+len = length(localPath);
+
+pathComponents = strsplit(localPath,filesep);
+
+fileNames = cellfun(@(x) x(len+2:end), Settings.ImageNamesList,...
+    'UniformOutput', false);
+
+folderName = strrep(pathComponents{end}, ' ', '\ ');
 
 command = ['mkdir ~/compute/OpenXY;mkdir ~/compute/OpenXY/' folderName];
 obj.connection = ssh2_command(obj.connection,command);
-obj.connection = scp_put(obj.connection, Settings.ImageNamesList,...
-    ['~/compute/OpenXY/' folderName]);
+obj.connection = scp_put(obj.connection, fileNames,...
+    ['~/compute/OpenXY/' folderName],localPath);
 end
