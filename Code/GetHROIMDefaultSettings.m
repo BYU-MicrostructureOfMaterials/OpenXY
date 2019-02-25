7d2d8f27a6fdf7b132847a8ec2bd35e2437fa0eb
David Fullwood <dfullwood@byu.edu>
2019-02-25 15:24:58 +0530

    Update GetHROIMDefaultSettings.m
    Fix default Reference Image Choice to 'Real'; previous version set to 'Real-Grain Ref', which is not a real option, but a combination of 'Real' and setting RefImageInd to 0.

--- Code/GetHROIMDefaultSettings.m
+++ Code/GetHROIMDefaultSettings.m
@@ -33,7 +33,7 @@ Settings.ImageFilter = [9 90 0 0];
 %% Advanced Settings
 %HROIM Settings
 Settings.DoStrain = 1;
-Settings.HROIMMethod = 'Real-Grain Ref'; %{'Simulated', 'Real'}, %Someday will add Hybrid
+Settings.HROIMMethod = 'Real';%Someday will add Hybrid
 Settings.IterationLimit = 6;
 Settings.RefImageInd = 0;
 Settings.StandardDeviation = 2;
