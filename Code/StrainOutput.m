1143f900560f748c9dafc9d238349d0df901ecef
David Fullwood <dfullwood@byu.edu>
2019-03-08 14:16:27 +0000

    Update StrainOutput.m
    Add effective (Von Mises) strain.
    Plot bad data black in effective strain plot, but should also be black in the other plots.
    Currently filters out poor data using only SSE deviation.

--- Code/StrainOutput.m
+++ Code/StrainOutput.m
@@ -52,11 +52,11 @@ end
 %Ignore obviously bad points
 SSE = Settings.SSE;
 BadIndex = 1:length(SSE);
-cutoff = (mean(SSE(SSE ~= inf)) + std(SSE(SSE ~= inf)));
+cutoff = (mean(SSE(SSE ~= inf)) + 2*std(SSE(SSE ~= inf)));
 BadPoints = SSE > cutoff;
 BadIndex = BadIndex(BadPoints);
 for j = 1:length(BadIndex)
-    data.F(:,:,BadIndex(j)) = eye(3); 
+    data.F(:,:,BadIndex(j)) = eye(3);
 end
 
 % FList = [data.F{:}];
@@ -162,8 +162,8 @@ for i=1:3
             caxis([smin smax])
             
             if r == 1
-               
-               
+                
+                
             end
             
             if DoShowGB && ~strcmp(Settings.ScanType,'Hexagonal')
@@ -192,4 +192,96 @@ for i=1:3
     end
 end
 
+% Plot Effective strain:
 
+% Von Mises Equivalent strain calculated from https://dianafea.com/manuals/d944/Analys/node405.html
+exx=(2*strain(1,1,:)-strain(2,2,:)-strain(3,3,:))/3;
+eyy=(-strain(1,1,:)+2*strain(2,2,:)-strain(3,3,:))/3;
+ezz=(-strain(1,1,:)-strain(2,2,:)+2*strain(3,3,:))/3;
+strainEff=2/3*sqrt(3*(exx.^2+eyy.^2+ezz.^2)/2+3*(strain(1,2,:).^2+strain(1,3,:).^2+strain(2,3,:).^2));
+
+if strcmp(Settings.ScanType,'Hexagonal')
+    NColsOdd = c;
+    NColsEven = c-1;
+    NRows = r;
+    count = 1;
+    
+    NewstrainEff = zeros(NRows,NColsEven);
+    if i==1 && j==1
+        gmap = zeros(NRows,NColsEven);
+    end
+    for rr = 1:NRows
+        if bitget(abs(rr),1)~=0 %odd
+            
+            for cc = 1:NColsOdd
+                NewstrainEff(rr,cc) = strainRff(count);
+                if i==1 && j==1
+                    gmap(rr,cc)=Settings.grainID(count);
+                end
+                count = count + 1;
+            end
+        else
+            for cc = 1:NColsEven
+                NewstrainEff(rr,cc) = strainEff(count);
+                if i==1 && j==1
+                    gmap(rr,cc)=Settings.grainID(count);
+                end
+                count = count + 1;
+            end
+        end
+    end
+    strainEff = NewstrainEff;
+    
+else
+    strainEff=reshape(strainEff, [c r])';
+    
+end
+
+if r == 1
+    strainEff = repmat(strainEff,Settings.ScanLength,1);
+end
+
+strainEffvec = map2vec(strainEff);
+AverageStrain = mean(strainEffvec(~BadPoints));
+%cMap = [[0 0 0];parula(126);[0 0 0]];
+cMap = parula(128);
+cMap(1,:) = cMap(1,:)./3;
+% cMap(end,:) = cMap(end,:)./3;
+if any(strcmp(['e' num2str(i) num2str(j)],Components))
+    figure;
+    imagesc(strainEff)
+    title(['Effective Strain, \epsilon_E_f_f Average Strain: ' num2str(AverageStrain),' (Black=Poor Data)'],'fontsize',14)
+    shading flat
+    axis equal tight
+    % view(2)
+    colorbar
+    colormap(cMap)
+    caxis([0 smax])
+    
+    if r == 1
+        
+        
+    end
+    
+    if DoShowGB && ~strcmp(Settings.ScanType,'Hexagonal')
+        if isempty(lines)% Cobbled together way to speed things up
+            lines = PlotGBs(Settings.grainID,[Settings.Nx Settings.Ny],Settings.ScanType);
+            if isfield(Settings, 'grainsHaveBeenSplit') && ...
+                    Settings.grainsHaveBeenSplit
+                redlines = PlotGBs(Settings.oldGrains.grainID, [Settings.Nx Settings.Ny], Settings.ScanType,gca,1.5,'r');
+            end
+        else
+            hold on
+            for ii = 1:size(lines,1)
+                plot(lines{ii,1},lines{ii,2},'LineWidth',1,'Color','k')
+            end%ii = 1:size(lines,1)
+            if isfield(Settings, 'grainsHaveBeenSplit') && ...
+                    Settings.grainsHaveBeenSplit
+                for ii = 1:size(redlines,1)
+                    plot(redlines{ii,1},redlines{ii,2},'LineWidth',1,'Color','r')
+                end%ii = 1:size(redlines,1)
+            end
+            hold off
+        end
+    end
+end
