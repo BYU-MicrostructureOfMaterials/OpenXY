3acb8cb16cb1c5dc64754a7afdf4d15a202a28df
Zach Clayburn <zachclayburn@gmail.com>
2019-07-16 16:46:53 -0600

    Fixed H5 HexGrid scans

--- Code/+patterns/H5PatternProvider.m
+++ Code/+patterns/H5PatternProvider.m
@@ -6,8 +6,10 @@ classdef H5PatternProvider < patterns.PatternProvider
     end
     
     properties(Access = private)
-        patternPath(1,:) char 
+        patternPath(1,:) char
         patternSize
+        
+        hexOffset
     end
     
     methods
@@ -17,7 +19,8 @@ classdef H5PatternProvider < patterns.PatternProvider
                 ['More than one root group in the HDF5 file %s,\n'...
                 'could not determine correct path to patterns.'], fileName);
             rootGroupName = fileInfo.Groups.Name;
-            patternPath = [rootGroupName '/EBSD/Data/Pattern'] ;
+            dataPath = [rootGroupName '/EBSD/Data'];
+            patternPath = [dataPath '/Pattern'];
             patternInfo = h5info(fileName, patternPath);
             patternSize = patternInfo.ChunkSize;
             
@@ -25,19 +28,50 @@ classdef H5PatternProvider < patterns.PatternProvider
             obj.patternPath = patternPath;
             obj.patternSize = patternSize;
             obj.imSize = patternSize(1:2);
+            
+            gridTypePath = [rootGroupName '/EBSD/Header/Grid Type'];
+            gridType = h5read(fileName, gridTypePath);
+            
+            validPath = [dataPath '/Valid'];
+            validVector = h5read(fileName, validPath);
+
+            switch deblank(gridType{1})
+                case 'HexGrid'
+                    offsetPoints = validVector == 2;
+                    scanLength = sum(~offsetPoints);
+                    obj.hexOffset = zeros(1, scanLength);
+                    
+                    offset = 0;
+                    for ii = 1:scanLength
+                        while offsetPoints(ii + offset)
+                            offset = offset + 1;
+                        end
+                        
+                        obj.hexOffset(ii) = offset;
+                    end
+                    
+                case 'SqrGrid'
+                    obj.hexOffset = zeros(1, length(validVector));
+                otherwise
+                    error('OpenXY:patterns:H5PatternProvider',...
+                        ['Unrecognized grid type!\n'...
+                        'Expected ''HexGrid'' or ''SqrGrid'',\n'...
+                        'got %s in stead'], gridType{1})
+            end
         end
         
         function sobj = saveobj(obj)
             sobj = saveobj@patterns.PatternProvider(obj);
             sobj.patternPath = obj.patternPath;
             sobj.patternSize = obj.patternSize;
+            sobj.hexOffset = obj.hexOffset;
         end
     end
     
     methods (Access = protected)
         function pattern = getPatternData(obj, ind)
             start = ones(size(obj.patternSize));
-            start(obj.patternSize == 1) = ind;
+            start(obj.patternSize == 1) = ind + obj.hexOffset(ind);
             count = obj.patternSize;
             pattern = h5read(obj.fileName, obj.patternPath, start, count)';
         end
@@ -48,9 +82,10 @@ classdef H5PatternProvider < patterns.PatternProvider
             obj = patterns.H5PatternProvider(loadStruct.fileName);
             obj.patternPath = loadStruct.patternPath;
             obj.patternSize = loadStruct.patternSize;
+            obj.hexOffset = loadStruct.hexOffset;
         end
         
-
+        
     end
 end
 
