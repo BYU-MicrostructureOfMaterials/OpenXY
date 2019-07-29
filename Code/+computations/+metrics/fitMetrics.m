2981f545a74d43b8f62a8bc30eacbc48bbe30c29
Zach Clayburn <zachclayburn@gmail.com>
2019-07-29 09:08:26 -0600

    Added new fit metric

--- Code/+computations/+metrics/fitMetrics.m
+++ Code/+computations/+metrics/fitMetrics.m
@@ -1,11 +1,21 @@
 function fitMetrics = fitMetrics(cx, cy, Cshift, Rshift, keepInds)
 
-if nargin == 0
+if nargin == 0 || (nargin == 1 && strcmp(cx, 'bad'))
     %If no arguments, return defalt 'null' values
     fitMetrics.SSE = inf;
     fitMetrics.rsqX = 0;
     fitMetrics.rsqY = 0;
+    fitMetrics.rsq = 0;
     return;
+end
+
+if nargin == 1 && strcmp(cx, 'good')
+    fitMetrics.SSE = 0;
+    fitMetrics.rsqX = 1;
+    fitMetrics.rsqY = 1;
+    fitMetrics.rsq = 1;
+    return;
+
 end
 
 usedCshift = Cshift(keepInds);
@@ -25,6 +35,16 @@ fitMetrics.SSE=sqrt(sum(...
 rsq_x = computations.metrics.rSquared(usedCshift, usedCx);
 rsq_y = computations.metrics.rSquared(usedRshift, usedCy);
 
+meanCShift = mean(usedCshift);
+meanRshift = mean(usedRshift);
+newShift = [(usedCshift - meanCShift) (usedRshift - meanRshift)];
+newC = [(usedCx - meanCShift) (usedCy - meanRshift)];
+
+rsq_all = computations.metrics.rSquared(newShift, newC);
+
 fitMetrics.rsqX = rsq_x;
 fitMetrics.rsqY = rsq_y;
+fitMetrics.rsq = rsq_all;
+
 end
+
