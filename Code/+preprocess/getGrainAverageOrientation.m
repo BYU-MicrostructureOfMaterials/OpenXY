7de221e795d2b8c704491f06b17f1495ca2302e5
Zach Clayburn <zachclayburn@gmail.com>
2019-08-05 10:48:30 -0600

    Grain averages works now, but screws up some of the time. Needs more work

---
+++ Code/+preprocess/getGrainAverageOrientation.m
@@ -0,0 +1,74 @@
+function [quats] = getGrainAverageOrientation(...
+    grainIDs, orientations, symOps)
+
+scanLength = length(grainIDs);
+
+
+if isequal(size(orientations), [scanLength, 3])
+    % Passed in euler angles
+    quats = euler2quat(orientations);
+elseif isequal(size(orientations), [scanLength, 4])
+    % Passed in quaterinons
+    quats = orientations;
+elseif isequal(size(orientations), [3, 3, scanLength])
+    % Passed in g matricies
+    quats = rmat2quat(orientations);
+else
+    error('OpenXY:GetGrainAverageOrientation', ...
+        ['Incorrect orientation shape. The argument ''orientations'' '...
+        'should be euler angles, g matricies or quaternions'])
+end
+
+sz = size(symOps);
+if numel(sz) == 3
+    if sz(1) ~= 3
+        symOps = permute(symOps, [2 3 1]);
+    end
+    symOps = rmat2quat(symOps);
+end
+
+numGrains = max(grainIDs);
+avgQuats = zeros(numGrains, 4);
+rotQuats = zeros(scanLength, 4);
+
+allAvg = rotQuats;
+
+for ii = 1:numGrains
+    currGrain = grainIDs == ii;
+    currQuats = quats(currGrain, :);
+    [avgQuats(ii, :), rotQuats(currGrain, :)] = getSymetryQuats(currQuats, symOps);
+    allAvg(currGrain, :) = repmat(avgQuats(ii, :), sum(currGrain), 1);
+end
+
+keyboard
+
+end
+
+function [avg_q, rot_quats] = getSymetryQuats(quats, symOps)
+%Still some problems on grains 2 & 3...
+
+refQuat = [
+    1 0 0 0
+    ];
+
+numQuats = size(quats, 1);
+symQuats = quatmult(symOps, quats, 'noreshape');
+symQuats = cat(3, symQuats, - symQuats);
+
+rot_quats = zeros(size(quats));
+
+for ii = 1:numQuats
+    curQuats = squeeze(symQuats(ii, :, :))';
+    misos = quatangle(refQuat, curQuats);
+    [~, minInd] = min(misos);
+    rot_quats(ii, :) = curQuats(minInd, :);
+end
+qMat = rot_quats' * rot_quats;
+[v, d] = eig(qMat);
+[~, maxInd] = max(max(d));
+
+avg_q = v(:, maxInd)';
+
+
+
+end
