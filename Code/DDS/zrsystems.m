007061cd80759224024e9ca63c8f236086a8a8cb
David Fullwood <dfullwood@byu.edu>
2019-09-10 21:15:38 -0600

    Update zrsystems.m

--- Code/DDS/zrsystems.m
+++ Code/DDS/zrsystems.m
@@ -1,25 +1,30 @@
 % dislocation systems of Zr
-% dtf 18-2-2019 Khushahal Thool
+% dtf 18-2-2019
 function [bedge,ledge, bscrew,lscrew,v,normals,crssfactor,type]=zrsystems;
-% 
-ctoa = 1.593; % 
+% a-type slip systems after Shahi thesis (McGill 2008), table 3.1
+ctoa = 1.593; % Shahi quotes Roberts as stating 1.624 at room temperature 
 a = 3.23e-10; % lattice parameter for Zr
 c=ctoa*a;
 v=0.35; %Poisson's ratio
-crsspris = 0.75; %factor for crss prismatic times that for basal% T.O. Erinosho, F.P.E. Dunne / International Journal of Plasticity 71 (2015) 170e194
-crsspyram =3.02; % is pyram a equal to pyramid ca?
-crsscapyram = 3.02;
-crssfactor=[ones(3,1);ones(3,1)*crsspris;ones(6,1)*crsspyram;ones(12,1)*crsscapyram]; %does pyramidal-a have same CRSS as pyramidal-ca??****
-type=[ones(1,12) zeros(1,12)];   %one for a-type, zero for ca-type
+crsspris = 0.92; %factor for crss prismatic times that for basal% T.O. Erinosho, F.P.E. Dunne / International Journal of Plasticity 71 (2015) 170e194
+crsspyram =7.69; % is pyram a equal to pyramid ca?
+crsscapyram = 1.23;
+%crssfactor=[ones(3,1);ones(3,1)*crsspris;ones(6,1)*crsspyram;ones(12,1)*crsscapyram]; %does pyramidal-a have same CRSS as pyramidal-ca??****
+crssfactor=[ones(3,1);ones(3,1)*crsspris;ones(12,1)*crsscapyram];
+twos=zeros(1,3);
+twos(1,:)=2; %Prismatic type
+threes=zeros(1,6);
+threes(1,:)=3;
+type=[ones(1,3),twos,zeros(1,12) ];   %one for basal-type, two for prismatic-type, three for pyramidal with a direction and zero for ca-type pyramidal
 
 
 
 norma=[0 0 0 1; 0 0 0 1; 0 0 0 1;   % basal
-    1 0 -1 0; -1 1 0 0; 0 -1 1 0;   % prismatic
-    1 0 -1 1; -1 0 1 1; -1 1 0 1; 1 -1 0 1; 0 -1 1 1; 0 1 -1 1];    %pyramidal
+    1 0 -1 0; -1 1 0 0; 0 -1 1 0];   % prismatic
+    %1 0 -1 1; -1 0 1 1; -1 1 0 1; 1 -1 0 1; 0 -1 1 1; 0 1 -1 1];    %pyramidal
 slipa=[-1 2 -1 0; -1 2 -1 0; 2 -1 -1 0;  % basal
-    -1 2 -1 0; -1 -1 2 0; 2 -1 -1 0;  % prismatic
-    -1 2 -1 0; -1 2 -1 0; -1 -1 2 0; -1 -1 2 0; 2 -1 -1 0; 2 -1 -1 0];    %pyramidal
+    -1 2 -1 0; -1 -1 2 0; 2 -1 -1 0];  % prismatic
+   % -1 2 -1 0; -1 2 -1 0; -1 -1 2 0; -1 -1 2 0; 2 -1 -1 0; 2 -1 -1 0];    %pyramidal
 normca=[1 0 -1 1; 1 0 -1 1; -1 1 0 1; -1 1 0 1; -1 1 0 1; -1 1 0 1; 1 -1 0 1; 1 -1 0 1; 0 -1 1 1; 0 -1 1 1; 0 1 -1 1; 0 1 -1 1];   %pyramidal c+a
 slipca=[-2 1 1 3; -1 -1 2 3; 2 -1 -1 3; 1 1 -2 3; 2 -1 -1 3; 1 -2 1 3; -2 1 1 3; -1 2 -1 3; -1 2 -1 3; 1 1 -2 3; 1 -2 1 3; -1 -1 2 3];   %pyramidal c+a
 
@@ -33,6 +38,7 @@ bca=slipca3; % c-a type burgers
 na=norma3; % a-type normals we are going to use
 nca=normca3;
 normals=[na;nca];
+
 
 
 lscrewa=ba; % line vectors for screw dislocations
